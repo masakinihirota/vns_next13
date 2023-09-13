08a0ed4ba055fbe877bf85e21e7233cea09bcbb3
masakinihirota <masakinihirota@gmail.com>
2023-09-14 00:48:01 +0900

    🎨 update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,56 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
 
-import type { NextRequest } from 'next/server'
+import type { NextRequest } from "next/server";
+
+import acceptLanguage from "accept-language";
+
+import { fallbackLng, languages, cookieName } from "@/app/i18n/settings";
+
+acceptLanguage.languages(languages);
+
+export const config = {
+  // matcher: '/:lng*'
+  matcher: ["/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)"],
+};
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
+  const res = NextResponse.next();
 
   // Create a Supabase client configured to use cookies
-  const supabase = createMiddlewareClient({ req, res })
+  const supabase = createMiddlewareClient({ req, res });
 
   // Refresh session if expired - required for Server Components
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
-  await supabase.auth.getSession()
+  await supabase.auth.getSession();
 
-  return res
+  let lng;
+  if (req.cookies.has(cookieName))
+    lng = acceptLanguage.get(req.cookies.get(cookieName)?.value);
+  if (!lng) lng = acceptLanguage.get(req.headers.get("Accept-Language"));
+  if (!lng) lng = fallbackLng;
+
+  // Redirect if lng in path is not supported
+  if (
+    !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
+    !req.nextUrl.pathname.startsWith("/_next")
+  ) {
+    return NextResponse.redirect(
+      new URL(`/${lng}${req.nextUrl.pathname}`, req.url)
+    );
+  }
+
+  const refererHeaderValue = req.headers.get("referer");
+
+  if (refererHeaderValue !== null) {
+    const refererUrl = new URL(refererHeaderValue);
+    const lngInReferer = languages.find((l) =>
+      refererUrl.pathname.startsWith(`/${l}`)
+    );
+    const res = NextResponse.next();
+    if (lngInReferer) res.cookies.set(cookieName, lngInReferer);
+    return res;
+  }
+
+  return res;
 }
