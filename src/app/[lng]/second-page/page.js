e7b5afc867b6f62e440bfc2064131ffb0509b81e
masakinihirota <masakinihirota@gmail.com>
2023-09-13 09:56:35 +0900

    ビルド成功 ライブラリのインストール move i18n [lng]

---
+++ src/app/[lng]/second-page/page.js
@@ -0,0 +1,22 @@
+import Link from "next/link"
+import { Footer } from "../components/Footer"
+import { useTranslation } from "../../i18n"
+
+export default async function Page({ params: { lng } }) {
+  const { t } = await useTranslation(lng, "second-page")
+
+  return (
+    <>
+      {/* <h1>Hi from second page!</h1> */}
+      <h1>{t("title")}</h1>
+      <Link href={`/${lng}`}>
+        {/* back */}
+        {t("back-to-home")}
+      </Link>
+      <br />
+      <Link href="/">トップ</Link>
+      <br />
+      <Footer lng={lng} />
+    </>
+  )
+}
