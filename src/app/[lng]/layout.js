cbd2967944072fef1612a49d95abede995154712
masakinihirota <masakinihirota@gmail.com>
2023-09-28 19:07:21 +0900

    ログインは各種機能ができるまで非表示

--- src/app/[lng]/layout.js
+++ src/app/[lng]/layout.js
@@ -1,20 +1,16 @@
 import { dir } from "i18next"
 
-import TopNav from "@/components/nav/TopNav"
-
 import { languages } from "../i18n/settings"
 
 export async function generateStaticParams() {
   return languages.map((lng) => ({ lng }))
 }
 
-export default function RootLayout({ children, params: { lng } }) {
+export default function Layout({ children, params: { lng } }) {
   return (
     // html要素のlang属性は、現在のドキュメントの言語を指定するために使用されます。
     // html要素のdir属性は、現在のドキュメントのテキスト方向を指定するために使用されます。
     <div lang={lng} dir={dir(lng)}>
-      <TopNav />
-
       {children}
     </div>
   )
--- src/components/nav/TopNav.tsx
+++ src/components/nav/TopNav.tsx
@@ -1,10 +1,20 @@
-// Nav.tsx
 import Link from "next/link"
 import React from "react"
 
 import { ThemeSwitcher } from "../ThemeSwitcher"
 
+// Login用
+// import LogoutButton from "../LogoutButton"
+// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+// import { cookies } from "next/headers"
+
 export default async function TopNav() {
+  // const supabase = createServerComponentClient({ cookies })
+
+  // const {
+  //   data: { user }
+  // } = await supabase.auth.getUser()
+
   return (
     <nav className="flex justify-center w-full h-16 border-b border-current">
       {/* nav部分 Home ,Advertisement, Dark mode,Language, Login */}
@@ -19,24 +29,24 @@ export default async function TopNav() {
         Language
         <div />
         Advertisement
+        {/* ログインは各種機能ができるまで非表示 */}
+        <div>
+          No-Account
+          {/* {user ? (
+            <div className="flex items-center gap-4">
+              Welcome to VNS.BLUE, {user.email}!
+              <LogoutButton />
+            </div>
+          ) : (
+            <Link
+              href="/login"
+              className="px-4 py-2 no-underline rounded-md bg-btn-background hover:bg-btn-background-hover"
+            >
+              Login
+            </Link>
+          )} */}
+        </div>
       </div>
-
-      {/* ログインは各種機能ができるまで非表示 */}
-      {/* <div>
-            {user ? (
-              <div className="flex items-center gap-4">
-                Welcome to VNS.BLUE, {user.email}!
-                <LogoutButton />
-              </div>
-            ) : (
-              <Link
-                href="/login"
-                className="px-4 py-2 no-underline rounded-md bg-btn-background hover:bg-btn-background-hover"
-              >
-                Login
-              </Link>
-            )}
-          </div> */}
     </nav>
   )
 }
